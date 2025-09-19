ebfec1d4088f708eb21402f7dae53375d13e8b08
Himanshu Thakar <127079822+himanshu21thakar@users.noreply.github.com>
2025-09-19 14:46:15 +0530

    Update create_database_mysql.py

--- create_database_mysql.py
+++ create_database_mysql.py
@@ -5,7 +5,7 @@ import pandas as pd
 connection = pymysql.connect(
     host='localhost',
     user='root',
-    password='himanshu@123',   # Change if needed
+    password='',   # Change if needed
     autocommit=True
 )
 
@@ -63,3 +63,4 @@ print("✅ BigMart database created and all tables loaded successfully!")
 # Close connection
 cursor.close()
 connection.close()
+
